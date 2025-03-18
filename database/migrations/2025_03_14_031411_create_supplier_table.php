f4551391694632b3d11a6d7467973ffeecfc5c77
Syifa Nur Kemala <152684508+gazelle61@users.noreply.github.com>
2025-03-18 09:35:40 +0700

    feat: [SNK] add db admin

--- database/migrations/2025_03_14_031411_create_supplier_table.php
+++ database/migrations/2025_03_14_031411_create_supplier_table.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
@@ -11,13 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('supplier', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nama_supplier');
+            $table->string('kontak_supplier', 20);
+            $table->string('kategori_supplier');
+            $table->text('barang_supplyan');
+            $table->text('alamat_supplier');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('supplier');
     }
 };
