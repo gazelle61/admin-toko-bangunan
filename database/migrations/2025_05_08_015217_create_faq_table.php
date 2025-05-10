6816f280f6636404e57f4ba0371656779e2649dd
Syifa Nur Kemala <152684508+gazelle61@users.noreply.github.com>
2025-05-10 13:44:12 +0700

    fix change schema db faq

--- database/migrations/2025_05_08_015217_create_faq_table.php
+++ database/migrations/2025_05_08_015217_create_faq_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('nama_pelanggan');
             $table->string('email')->unique();
             $table->text('tanya');
-            $table->text('jawab');
             $table->timestamps();
         });
     }
